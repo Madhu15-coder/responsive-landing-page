1dee72a0e3c8dab2d44036195758fba486a1ba81
Madhu15-coder <madhunarsale4282@gmail.com>
2025-06-15 13:41:11 +0530

    Add script.js for navbar scroll effect

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript to change navbar background on scroll
+window.addEventListener('scroll', function () {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.style.backgroundColor = '#222';
+  } else {
+    navbar.style.backgroundColor = '#333';
+  }
+});
